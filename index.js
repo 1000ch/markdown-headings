2b241e1fc08388c57ad80b2505b2875c90914d01
1000ch <shogo.sensui@gmail.com>
2018-07-03 19:02:56 +0900

    Pass a required argument

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = arg => {
   }
 
   const markdownIt = new MarkdownIt();
-  const tokens = markdownIt.parse(markdown);
+  const tokens = markdownIt.parse(markdown, {});
 
   const headings = [];
   let headingToken = null;
