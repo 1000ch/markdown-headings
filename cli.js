1fa54b1a258d9334d1dc4fc7a0b5ebc6c64cb8aa
1000ch <shogo.sensui@gmail.com>
2018-07-04 01:17:40 +0900

    Print filename

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ const fsP = require('pify')(fs);
 const minimist = require('minimist');
 const globby = require('globby');
 const getStdin = require('get-stdin');
-const markdownHeadings = require('.');
+const m = require('.');
 
 const getVersion = () => Promise.resolve(require('./package').version);
 
@@ -14,10 +14,6 @@ const getHelp = () => fsP.readFile(`${__dirname}/usage.txt`).then(b => b.toStrin
 
 const getFiles = args => globby(args, {nodir: true})
   .then(files => files.map(file => path.resolve(process.cwd(), file)));
-
-const getResults = files => Promise.all(files.map(file => fsP.readFile(file)))
-  .then(texts => Promise.all(texts.map(text => markdownHeadings(text))))
-  .then(results => Array.prototype.concat.apply([], results));
 
 const argv = minimist(process.argv.slice(2), {
   alias: {
@@ -37,16 +33,24 @@ if (argv.v || argv.version) {
 } else if (argv.h || argv.help) {
   getHelp().then(help => console.log(help));
 } else if (argv.stdin) {
-  getStdin().then(string => markdownHeadings(string)).then(headings => {
+  getStdin().then(string => m(string)).then(headings => {
     console.log(headings.join('\n'));
     process.exit(0);
   });
 } else {
-  getFiles(argv._).then(files => getResults(files)).then(headings => {
-    console.log(headings.join('\n'));
-    process.exit(0);
-  }).catch(err => {
-    console.error(err);
-    process.exit(1);
+  getFiles(argv._).then(files => {
+    Promise.all(files.map(file => {
+      return fsP.readFile(file).then(text => m(text)).then(headings => {
+        headings.unshift(path.basename(file));
+
+        return headings;
+      });
+    })).then(headings => {
+      console.log([].concat(...headings).join('\n'));
+      process.exit(0);
+    }).catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
   });
 }
